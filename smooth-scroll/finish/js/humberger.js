f53f9a215c1549000a0029db7e248cce19447367
mitsuhashi <isao10201020@yahoo.co.jp>
2020-07-28 12:12:28 +0900

    Add files via upload

---
+++ smooth-scroll/finish/js/humberger.js
@@ -0,0 +1,9 @@
+$(function() {
+
+  $('.menu-trigger').on('click', function() {
+    $(this).toggleClass('active');
+    $('#global-nav').toggleClass('active');
+    //$(this).next().toggleClass('active');
+  });
+
+});
---
+++ smooth-scroll/finish/js/script.js
@@ -0,0 +1,7 @@
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    header: '#global-nav',
+    // easing: 'easeInOutCubic',//イージング
+    // speed: 1000,
+});
